4badd865150ee7d8dc473ed3aa4920af16d20d47
Jonathon Day <jonathonpday@me.com>
2021-10-13 23:31:26 +0100

    Test scripts

---
+++ learn_to_program/age_in_seconds.rb
@@ -0,0 +1,13 @@
+# A simple program to caulate my age in seconds
+# Current Age: 35 years
+#
+
+hour = 60 * 60
+
+day = hour * 24
+
+year = day * 360
+
+age = 35 * year
+
+puts age
---
+++ learn_to_program/minutes_in_decade.rb
@@ -0,0 +1,9 @@
+# A script to calculate tyhe number of minutes in a decade
+
+day = 60 * 24
+
+year = day * 360
+
+decade = year * 10
+
+puts decade
