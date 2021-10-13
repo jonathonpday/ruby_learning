16fdfd46b0bf391e8729639f8a5b447ee42db6ec
Jonathon Day <jonathonpday@me.com>
2021-10-13 23:38:48 +0100

    Completed first chapter

---
+++ learn_to_program/how_old.rb
@@ -0,0 +1,13 @@
+# A script to determine how many years old the author is (given as 1390 milliom seconds)
+
+seconds = 1390000000
+
+minutes  = seconds / 60
+
+hours = minutes / 60
+
+days = hours / 24
+
+years = days / 360
+
+puts years
